89d715dcaa9445dec860309a05a39725bb8fac57
molodkov yaroslav <yaroslav.molodkov@odysseusinc.com>
2019-10-04 15:46:08 +0200

    ARACHNE-2723 IR fails on BigQuery (#2)
    * ARACHNE-2723 IR fails on BigQuery
    - pass result schema as temp schema to the SqlRender
    - replace deprecated translateSql method with translate
    
    * ARACHNE-2723 IR fails on BigQuery (fix PR remarks)
    - use render instead of deprecated renderSql
    - add tempDatabaseSchema as default argument in method

--- R/run_ir_analysis.r
+++ R/run_ir_analysis.r
@@ -1,6 +1,6 @@
 #
 #' @export
-run_ir_analysis <- function(basicDir, analysisId, cohortDefinitions, dbms, connectionString, user, password, driversPath, cdmDatabaseSchema, resultsDatabaseSchema, cohortsDatabaseSchema, cohortTable = "cohort", outcomeTable = "cohort"){
+run_ir_analysis <- function(basicDir, analysisId, cohortDefinitions, dbms, connectionString, user, password, driversPath, cdmDatabaseSchema, resultsDatabaseSchema, cohortsDatabaseSchema, tempDatabaseSchema = resultsDatabaseSchema, cohortTable = "cohort", outcomeTable = "cohort"){
   start.time <- Sys.time()
   library(SqlRender)
   library(DatabaseConnector)
@@ -17,8 +17,8 @@ run_ir_analysis <- function(basicDir, analysisId, cohortDefinitions, dbms, conne
   connection <- DatabaseConnector::connect(connectionDetails)
 
   query <- SqlRender::readSql(system.file("sql/sql_server", "delete_strata.sql", package = "IncidenceRateSkeleton"))
-  query <- SqlRender::renderSql(query, tableQualifier = resultsDatabaseSchema, analysis_id = analysisId)$sql
-  query <- SqlRender::translateSql(query, targetDialect = connectionDetails$dbms)$sql
+  query <- SqlRender::render(query, tableQualifier = resultsDatabaseSchema, analysis_id = analysisId)
+  query <- SqlRender::translate(query, targetDialect = connectionDetails$dbms, oracleTempSchema=tempDatabaseSchema)
 
   DatabaseConnector::executeSql(connection, query)
 
@@ -26,13 +26,13 @@ run_ir_analysis <- function(basicDir, analysisId, cohortDefinitions, dbms, conne
   for(cohortFile in cohortDefinitions){
     cf <- file.path(basicDir, cohortFile)
     sql <- readSql(cf)
-    sql <- renderSql(sql,
+    sql <- render(sql,
                      cdm_database_schema = cdmDatabaseSchema,
                      vocabulary_database_schema = cdmDatabaseSchema,
                      target_database_schema = cohortsDatabaseSchema,
                      target_cohort_table = cohortTable,
-                     output = "output")$sql
-    sql <- translateSql(sql, targetDialect = connectionDetails$dbms)$sql
+                     output = "output")
+    sql <- translate(sql, targetDialect = connectionDetails$dbms, oracleTempSchema=tempDatabaseSchema)
     executeSql(connection, sql)
   }
 
@@ -43,45 +43,45 @@ run_ir_analysis <- function(basicDir, analysisId, cohortDefinitions, dbms, conne
   for (i in seq_along(strataRules)) {
     strata <- strataRules[i]
     query <- SqlRender::readSql(system.file("sql/sql_server", "strata_rules.sql", package = "IncidenceRateSkeleton"))
-    query <- SqlRender::renderSql(query, results_schema = resultsDatabaseSchema,
+    query <- SqlRender::render(query, results_schema = resultsDatabaseSchema,
                                   analysis_id = analysisId,
                                   strata_sequence = i,
                                   name = strata[[1]]$name,
-                                  description = "")$sql
-    query <- SqlRender::translateSql(query, targetDialect = connectionDetails$dbms)$sql
+                                  description = "")
+    query <- SqlRender::translate(query, targetDialect = connectionDetails$dbms, oracleTempSchema=tempDatabaseSchema)
     DatabaseConnector::executeSql(connection, query)
   }
 
   #source('ir_analysis_query_builder.r')
   expressionSql <- buildAnalysisQuery(analysisDescription, analysisId, dbms, cdmDatabaseSchema, resultsDatabaseSchema)
-  translatedSql <- translateSql(expressionSql, targetDialect = dbms)$sql
+  translatedSql <- translate(expressionSql, targetDialect = dbms, oracleTempSchema=tempDatabaseSchema)
   DatabaseConnector::executeSql(connection, translatedSql)
 
   # Save results
   # Summary
   sql <- SqlRender::readSql(system.file("sql/sql_server", "analysis_summary.sql", package = "IncidenceRateSkeleton"))
-  sql <- SqlRender::renderSql(sql,
+  sql <- SqlRender::render(sql,
                    resultsSchema = resultsDatabaseSchema,
-                   id = analysisId)$sql
-  sql <- SqlRender::translateSql(sql, targetDialect = dbms)$sql
+                   id = analysisId)
+  sql <- SqlRender::translate(sql, targetDialect = dbms, oracleTempSchema=tempDatabaseSchema)
   result <- DatabaseConnector::querySql(connection, sql)
   write.csv(result, file.path(workDir, "ir_summary.csv"), na = "")
 
   # Dist
   sql <- SqlRender::readSql(system.file("sql/sql_server", "ir_dist.sql", package = "IncidenceRateSkeleton"))
-  sql <- SqlRender::renderSql(sql,
+  sql <- SqlRender::render(sql,
                               resultsSchema = resultsDatabaseSchema,
-                              analysisId = analysisId)$sql
-  sql <- SqlRender::translateSql(sql, targetDialect = dbms)$sql
+                              analysisId = analysisId)
+  sql <- SqlRender::translate(sql, targetDialect = dbms, oracleTempSchema=tempDatabaseSchema)
   dist <- DatabaseConnector::querySql(connection, sql)
   write.csv(dist, file.path(workDir, "ir_dist.csv"), na = "")
 
   # Strata
   sql <- SqlRender::readSql(system.file("sql/sql_server", "strata_stats.sql", package = "IncidenceRateSkeleton"))
-  sql <- SqlRender::renderSql(sql,
+  sql <- SqlRender::render(sql,
                             results_database_schema = resultsDatabaseSchema,
-                            analysis_id = analysisId)$sql
-  sql <- SqlRender::translateSql(sql, targetDialect = dbms)$sql
+                            analysis_id = analysisId)
+  sql <- SqlRender::translate(sql, targetDialect = dbms, oracleTempSchema=tempDatabaseSchema)
   strata <- DatabaseConnector::querySql(connection, sql)
   write.csv(strata, file.path(workDir, "ir_strata.csv"), na = "")
 
